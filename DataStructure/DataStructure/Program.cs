c46943b802a476d929a2e18108358702722dc7ec
Nitin308 <nitin3081998@gmail.com>
2022-12-07 19:59:59 +0530

    Adding more elements in a binary tree and checking the size

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree");
+
+            BinarySearchTree<int> objBinary = new BinarySearchTree<int>(56);
+            objBinary.Add(30);
+            objBinary.Add(70);
+            objBinary.Add(22);
+            objBinary.Add(40);
+            objBinary.Add(60);
+            objBinary.Add(95);
+            objBinary.Add(11);
+            objBinary.Add(65);
+            objBinary.Add(3);
+            objBinary.Add(16);
+            objBinary.Add(63);
+            objBinary.Add(67);
+            Console.WriteLine("\nSorted Binary Tree ==>");
+            objBinary.Display();
+            objBinary.Size();
+            Console.ReadLine();
+        }
+    }
+}
