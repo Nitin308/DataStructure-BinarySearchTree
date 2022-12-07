c1e255ff7250eca1a1b8d6f337188970e180ea29
Nitin308 <nitin3081998@gmail.com>
2022-12-07 20:10:15 +0530

    Search an element in a Binary Tree

---
+++ DataStructure/DataStructure/BinarySearchTree.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataStructure
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> LeftTree { get; set; }
+        public BinarySearchTree<T> RightTree { get; set; }
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        int leftCount = 0;
+        int rightCount = 0;
+
+        public void Add(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new BinarySearchTree<T>(item);
+                    leftCount++;
+                }
+                else
+                {
+                    this.LeftTree.Add(item);
+                    leftCount++;
+                    return;
+                }
+            }
+            else
+            {
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new BinarySearchTree<T>(item);
+                    rightCount++;
+                }
+                else
+                {
+                    this.RightTree.Add(item);
+                    rightCount++;
+                    return;
+                }
+            }
+            Console.WriteLine($"Added : {item}");
+        }
+
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.LeftTree.Display();
+            }
+            Console.Write(this.NodeData.ToString() + " ");
+            if (this.RightTree != null)
+            {
+                this.RightTree.Display();
+            }
+        }
+
+        public void Size()
+        {
+            Console.WriteLine($"\n\nSize of Binary Tree : {(1 + leftCount + rightCount)}");
+        }
+
+        public BinarySearchTree<T> FindElement(T element, BinarySearchTree<T> node)
+        {
+            if (node != null)
+            {
+                if (element.Equals(node.NodeData))
+                {
+                    return node;
+                }
+                if (element.CompareTo(node.NodeData) < 0)
+                {
+                    return FindElement(element, node.LeftTree);
+                }
+                else
+                {
+                    return FindElement(element, node.RightTree);
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree");
+
+            BinarySearchTree<int> objBinary = new BinarySearchTree<int>(56);
+            objBinary.Add(30);
+            objBinary.Add(70);
+            objBinary.Add(22);
+            objBinary.Add(40);
+            objBinary.Add(60);
+            objBinary.Add(95);
+            objBinary.Add(11);
+            objBinary.Add(65);
+            objBinary.Add(3);
+            objBinary.Add(16);
+            objBinary.Add(63);
+            objBinary.Add(67);
+
+            Console.WriteLine("\nSorted Binary Tree ==>");
+            objBinary.Display();
+
+            objBinary.Size();
+
+            BinarySearchTree<int> node = objBinary.FindElement(63, objBinary);
+            Console.WriteLine($"\nElement is : {node.NodeData}");
+
+            Console.ReadLine();
+        }
+    }
+}
